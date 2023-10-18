27b841ef4bbf7757a9eeb47df295fd71fdba244b
Fody <130543954+FodyF@users.noreply.github.com>
2023-10-18 14:23:59 +1300

    Adding its override

--- __no commit/ref/should.js
+++
@@ -1,8 +0,0 @@
-
-Cypress.Commands.overwrite('should', function (originalFn, ...args) {
-  const subject = args[0]
-  if (subject === null) {
-    return () => null
-  }
-  return originalFn.apply(this, args)
-})
